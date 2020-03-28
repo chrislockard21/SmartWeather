f49fb4bedec2b2ebb3dc024cbe3d6fa2ab2330b1
Casey P <62313590+pavers355@users.noreply.github.com>
2020-03-27 20:12:53 -0400

    link to new cobra map
    updated map link

--- smart_weather/weather/static/weather/js/main.js
+++ smart_weather/weather/static/weather/js/main.js
@@ -3,7 +3,7 @@ function getWeatherLocation() {
     const latitude  = position.coords.latitude.toPrecision(6);
     const longitude = position.coords.longitude.toPrecision(6);
 
-    document.querySelector('#map').src = `https://cobra.maps.arcgis.com/apps/webappviewer/index.html?id=ee70f4d7437d47bb88cf9de9eb4f8719&find=${longitude},${latitude}`;
+    document.querySelector('#map').src = `//cobra.maps.arcgis.com/apps/Embed/index.html?webmap=c4fcd13aa52e4dcfb24cc6e90a970a59&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light&marker=${longitude},${latitude}&center=${longitude},${latitude}&level=10`;
   }
 
   if (navigator.geolocation) {
@@ -13,7 +13,7 @@ function getWeatherLocation() {
 
 function getWeatherLocationByZip() {
   const zip  = document.querySelector('#enterZip').value;
-  document.querySelector('#map').src = `https://cobra.maps.arcgis.com/apps/webappviewer/index.html?id=ee70f4d7437d47bb88cf9de9eb4f8719&find=${zip}`;
+  document.querySelector('#map').src = `//cobra.maps.arcgis.com/apps/Embed/index.html?webmap=c4fcd13aa52e4dcfb24cc6e90a970a59&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light&center=${zip}&find=${zip}&level=8`;
 }
 
 window.onload = function() {
